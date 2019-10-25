7a43425465cc1667b02c04085ac892667c9de0f7
Luiz Martins <43974104+LuizOMartins@users.noreply.github.com>
2019-10-25 07:26:13 -0300

    Verso PUT
    Verso PUT

--- teste-API/src/main/java/com/example/demo/repository/PessoaRepository.java
+++ teste-API/src/main/java/com/example/demo/repository/PessoaRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+	void save(Optional<Pessoa> pessoaSalva);
+
 
 }
