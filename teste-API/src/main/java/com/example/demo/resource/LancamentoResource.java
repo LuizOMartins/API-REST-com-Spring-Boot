1aca1b50b284b57edd29b4fdd8d89a97460a004c
Luiz Eduardo <43974104+LuizOMartins@users.noreply.github.com>
2020-01-05 22:56:16 -0300

    Validando inconsistencias Lancamento

--- teste-API/src/main/java/com/example/demo/resource/LancamentoResource.java
+++ teste-API/src/main/java/com/example/demo/resource/LancamentoResource.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.event.RecursoCriadoEvent;
 import com.example.demo.model.Lancamento;
-import com.example.demo.model.Pessoa;
 import com.example.demo.repository.LancamentoRepository;
 
 @RestController
